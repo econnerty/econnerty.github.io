5f64e1ceebdd34049091f28c8a14f70c147c95cc
Erik Connerty <erikc@email.sc.edu>
2023-04-16 02:54:36 -0400

    code

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  Box,
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import useScroll from '../hooks/useScroll';
 
 const Navigation = () => {
@@ -14,36 +22,52 @@ const Navigation = () => {
 
   return (
     <AppBar position="fixed" sx={appBarStyle} elevation={0}>
-      <Toolbar>
+      <Toolbar
+        sx={{
+          flexDirection: { xs: 'column', sm: 'row' },
+          justifyContent: { xs: 'center', sm: 'space-between' },
+        }}
+      >
         <Typography
           variant="h1"
           sx={{
-            flexGrow: 1,
-            fontWeight:'bold',
+            fontWeight: 'bold',
             color: 'secondary.main',
             fontFamily: '"Playfair Display", serif',
+            mb: { xs: 1, sm: 0 },
           }}
         >
           Erik Connerty
         </Typography>
-        <Button color="inherit" component={NavLink} to="/" exact>
-          Home
-        </Button>
-        <Button color="inherit" component={NavLink} to="/about">
-          About
-        </Button>
-        <Button color="inherit" component={NavLink} to="/CV">
-          CV
-        </Button>
-        <Button color="inherit" component={NavLink} to="/projects">
-          Projects
-        </Button>
-        <Button color="inherit" component={NavLink} to="/contact">
-          Contact
-        </Button>
+        <Box sx={{ display: { xs: 'block', sm: 'none' } }}>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+        </Box>
+        <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+          <Button color="inherit" component={NavLink} to="/" exact>
+            Home
+          </Button>
+          <Button color="inherit" component={NavLink} to="/about">
+            About
+          </Button>
+          <Button color="inherit" component={NavLink} to="/CV">
+            CV
+          </Button>
+          <Button color="inherit" component={NavLink} to="/projects">
+            Projects
+          </Button>
+          <Button color="inherit" component={NavLink} to="/contact">
+            Contact
+          </Button>
+        </Box>
       </Toolbar>
     </AppBar>
   );
 };
-
 export default Navigation;
