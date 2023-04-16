ce950ede340defc33bbd1b2c4f3af871b09a5259
Erik Connerty <erikc@email.sc.edu>
2023-04-16 15:05:33 -0400

    hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import Home from './pages/Home';
 import About from './pages/About';
